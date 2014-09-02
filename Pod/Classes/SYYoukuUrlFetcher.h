0db235c836bdcfd3865cacef4c18167af395fa83
Sherlock Yao <yaosherlock@gmail.com>
2014-09-02 15:07:36 +0800

    demo version

--- Pod/Classes/SYYoukuUrlFetcher.h
+++ Pod/Classes/SYYoukuUrlFetcher.h
@@ -8,6 +8,140 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ * The response data of GET http://v.youku.com/player/getPlayList/VideoIDS/{video id} looks like:
+ *{
+   "data": [
+     {
+       "ct": "t",
+       "cs": "250",
+       "logo": "http://g3.ykimg.com/1100641F46518B6D265F7C089F1EFF1C66DB2E-E71A-BE2D-89DE-FBADB880F58D",
+       "seed": 7292,
+       "tags": [
+         "留学",
+         "美国留学",
+         "申请",
+         "考试辅导",
+         "语言学习",
+         "英国留学",
+         "ChaseFuture",
+         "剑桥",
+         "个人陈述",
+         "Workshop"
+       ],
+       "categories": "87",
+       "videoid": "138605909",
+       "vidEncoded": "XNTU0NDIzNjM2",
+       "username": "Chase_Future",
+       "userid": "144645887",
+       "title": "ChaseFuture留学指导Workshop第五课：留学申请与志愿者经历",
+       "up": 0,
+       "down": 0,
+       "ts": "nCjSCTEwA*E50ToFAT-qG60",
+       "tsup": "nCjWHzIwA*E50ToFAinuG60",
+       "preview": {
+         "thumbs": [
+           "05210001518B6D546A0737503402AC37"
+         ],
+         "sectiontime": "6000",
+         "host": "http://g2.ykimg.com/"
+       },
+     "key1": "b3477a1c",
+     "key2": "363569b1e407509c",
+     "tt": "0",
+     "videoSource": "1",
+     "seconds": "537.20",
+     "streamfileids": {
+       "flv": "64*51*64*64*64*11*64*11*64*64*62*1*30*38*0*31*62*11*62*42*38*1*64*30*27*24*1*42*24*24*62*31*51*37*31*64*1*60*22*64*38*30*51*22*24*62*64*23*22*60*37*24*60*22*1*60*38*27*0*27*5*11*64*60*42*23*",
+       "mp4": "64*51*64*64*64*30*64*11*64*64*62*1*30*38*0*60*11*51*62*42*38*1*64*30*27*24*1*42*24*24*62*31*51*37*31*64*1*60*22*64*38*30*51*22*24*62*64*23*22*60*37*24*60*22*1*60*38*27*0*27*5*11*64*60*42*23*"
+     },
+     "segs": {
+       "flv": [
+         {
+           "no": "0",
+           "size": "8871647",
+           "seconds": 253,
+           "k": "c00d335ae78dcc242411eca2",
+           "k2": "122476ee2d10f69dd"
+         },
+         {
+           "no": "1",
+           "size": "10420822",
+           "seconds": 284,
+           "k": "1c769df6012b02622829d2d1",
+           "k2": "1c39129ad70a4ed6f"
+         }
+       ],
+       "mp4": [
+         {
+           "no": "0",
+           "size": "18246602",
+           "seconds": 274,
+           "k": "0bf928c1d330fc152829d2d1",
+           "k2": "1fbe8e221bfda68c6"
+         },
+         {
+           "no": "1",
+           "size": "18715326",
+           "seconds": 263,
+           "k": "59eab2059b3315f42411eca2",
+           "k2": "1ae89985b7c6bc358"
+         }
+       ]
+     },
+     "streamsizes": {
+       "flv": "19292469",
+       "mp4": "36961928"
+     },
+     "stream_ids": {
+       "flv": "125651719",
+       "mp4": "125652516"
+     },
+     "streamlogos": {
+       "flv": 1,
+       "mp4": 1
+     },
+     "streamtypes": [
+       "flv",
+       "mp4"
+     ],
+     "streamtypes_o": [
+       "flvhd",
+       "mp4"
+     ]
+   }
+   ],
+   "user": {
+     "id": "16080280"
+   },
+   "controller": {
+     "search_count": true,
+     "mp4_restrict": 1,
+     "stream_mode": 1,
+     "video_capture": true,
+     "hd3_enabled": false,
+     "area_code": 330100,
+     "dma_code": 4837,
+     "continuous": 0,
+     "playmode": "normal",
+     "circle": false,
+     "tsflag": false,
+     "other_disable": false,
+     "xplayer_disable": false,
+     "app_disable": false,
+     "share_disabled": false,
+     "download_disabled": false,
+     "pc_disabled": false,
+     "pad_disabled": false,
+     "mobile_disabled": false,
+     "tv_disabled": false,
+     "comment_disabled": false
+   }
+ }
+ *
+ **/
 @interface SYYoukuUrlFetcher : NSObject
+
+- (void)urlOfId:(NSString *)videoId;
 
 @end
--- Pod/Classes/SYYoukuUrlFetcher.m
+++ Pod/Classes/SYYoukuUrlFetcher.m
@@ -7,7 +7,90 @@
 //
 
 #import "SYYoukuUrlFetcher.h"
+#import "AFNetworking.h"
+
+@interface SYYoukuUrlFetcher ()
+
+@property (nonatomic, strong) NSOperationQueue *requestQueue;
+
+@end
 
 @implementation SYYoukuUrlFetcher
+
+- (id)init {
+  if (self = [super init]) {
+    _requestQueue = [[NSOperationQueue alloc] init];
+  }
+  return self;
+}
+
+- (void)urlOfId:(NSString *)videoId {
+  NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://v.youku.com/player/getPlayList/VideoIDS/XNTU0NDIzNjM2"]];
+  AFHTTPRequestOperation *requestOp = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+  requestOp.responseSerializer = [AFJSONResponseSerializer serializer];
+  [requestOp setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+    [self finishRequestWithData:responseObject];
+  } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+    //TODO: failed
+  }];
+  [self.requestQueue addOperation:requestOp];
+}
+
+- (void)finishRequestWithData:(NSDictionary *)data {
+  //TODO: parse error handle, check nulls
+  NSDictionary *videoData = [data valueForKey:@"data"][0];
+  NSInteger seed = [[videoData valueForKey:@"seed"] integerValue];
+  NSString *fileId = [[videoData valueForKey:@"streamfileids"] valueForKey:@"mp4"];
+  NSString *realId = [self getFileId:fileId seed:seed];
+  NSString *leftId = [realId substringToIndex:8];
+  NSString *rightId = [realId substringFromIndex:10];
+  NSArray *segs = [[videoData valueForKeyPath:@"segs"] valueForKey:@"mp4"];
+  NSMutableString *videoUrl = [NSMutableString string];
+  NSString *sid = [self getSid];
+  for (int i = 0; i < [segs count]; i++) {
+    NSDictionary *seg = segs[i];
+    NSString *k = [seg valueForKey:@"k"];
+    NSString *index = [NSString stringWithFormat:@"%02x", i];
+    NSString *url = [NSString stringWithFormat:@"http://f.youku.com/player/getFlvPath/sid/%@_%@/st/%@/fileid/%@%@%@?K=%@", sid, index, @"mp4", leftId, index, rightId, k];
+    [videoUrl appendString:url];
+    [videoUrl appendString:@"#"];
+  }
+  NSLog(@"===========================> URL:\n%@", videoUrl);
+  //TODO: report the videoUrl to delegate
+}
+
+
+- (NSString *)getFileId:(NSString *)fileId seed:(NSInteger)seed {
+  NSString *mixed = [self getFileIdMixString:seed];
+  NSArray *ids = [fileId componentsSeparatedByString:@"\\*"];
+  NSMutableString *result = [NSMutableString string];
+  for (int i = 0; i < [ids count]; i++) {
+    NSInteger index = [ids[i] integerValue];
+    NSRange range = NSMakeRange(index, 1);
+    [result appendString:[mixed substringWithRange:range]];
+  }
+  return [NSString stringWithString:result];
+}
+
+- (NSString *)getSid {
+  long i1 = (long) (1000 + floor(arc4random() * 999));
+  long i2 = (long) (1000 + floor(arc4random() * 9000));
+  long t = [[NSDate date] timeIntervalSince1970] * 1000;
+  return [NSString stringWithFormat:@"%lu%lu%lu", t, i1, i2];
+}
+
+- (NSString *)getFileIdMixString:(NSInteger)seed {
+  NSMutableString *source = [NSMutableString stringWithString:@"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ/\\:._-1234567890"];
+  NSMutableString *mixed = [NSMutableString string];
+  NSInteger length = source.length;
+  for (int i = 0; i < length; i++) {
+    seed = (seed * 211 + 30031) % 65536;
+    NSInteger index = floor(seed / 65536 * source.length);
+    NSRange range = NSMakeRange(index, 1);
+    [mixed appendString:[source substringWithRange:range]];
+    [source deleteCharactersInRange:range];
+  }
+  return [NSString stringWithString:mixed];
+}
 
 @end
